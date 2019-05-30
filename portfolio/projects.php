b94827d53eae5a4a45d21737ea2ebe7daf1170aa
Dragonhost <41574563+Dragonhost@users.noreply.github.com>
2019-05-30 22:57:19 +0530

    Added the cards

--- portfolio/projects.php
+++ portfolio/projects.php
@@ -1,4 +1,35 @@
 <?php  require 'html/head.php' ?>
 <?php  require 'html/nav.php' ?>
 
+
+<div class="cards">
+    <div class="card">
+        <div class="title">
+            <h1>Disappr</h1>
+        </div>
+        <div class="text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
+            </p>            
+        </div>
+        <div class="links">
+            <a href="">Source Code</a>
+            <a href="">View Project</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="title">
+            <h1>Disappr</h1>
+        </div>
+        <div class="text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
+            </p>            
+        </div>
+        <div class="links">
+            <a href="">Source Code</a>
+            <a href="">View Project</a>
+        </div>
+    </div>
+</div>
+<br><br><br><br>
+
 <?php  require 'html/footer.php' ?>
