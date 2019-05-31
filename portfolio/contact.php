d38e4f8b76711d152b5704c41ef3526998925539
Dragonhost <kamaltejag@gmail.com>
2019-05-31 16:56:39 +0530

    Updated Portfolio

--- portfolio/contact.php
+++ portfolio/contact.php
@@ -1,4 +1,22 @@
 <?php  require 'html/head.php' ?>
-<?php  require 'html/nav.php' ?>
 
-<?php  require 'html/footer.php' ?>
+<div class="contect_container">
+    <?php  require 'html/nav.php' ?>
+    <div class="contact_box">
+        <div class="contact_text">
+            <h1 class="contact">Contact</h1>
+            <div class="contact_links">
+                <a href="https://github.com/Dragonhost/Dragonhost.github.io" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/kamal-teja-157111168/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://www.facebook.com/kamal.gaajula" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            </div>
+        </div>
+        <form action="contact.php" class="contact_form">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Email">
+            <textarea placeholder="Message"></textarea>
+        </form>
+    </div>
+
+    <?php  require 'html/footer.php' ?>
+</div>
--- portfolio/projects.php
+++ portfolio/projects.php
@@ -1,35 +1,50 @@
 <?php  require 'html/head.php' ?>
-<?php  require 'html/nav.php' ?>
 
-
-<div class="cards">
-    <div class="card">
-        <div class="title">
-            <h1>Disappr</h1>
-        </div>
-        <div class="text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
-            </p>            
-        </div>
-        <div class="links">
-            <a href="">Source Code</a>
-            <a href="">View Project</a>
+<div class="projects_box">
+    <?php  require 'html/nav.php' ?>
+    <div class="projects">
+        <h1 class="projects_heading">Projects</h1>
+        <div class="cards">
+            <div class="card">
+                <div class="title">
+                    <h1>Disappr</h1>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
+                    </p>            
+                </div>
+                <div class="links">
+                    <a href=""class="source">Source Code</a>
+                    <a href="" class="view">View Project &raquo;</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="title">
+                    <h1>Disappr</h1>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
+                    </p>            
+                </div>
+                <div class="links">
+                    <a href=""class="source">Source Code</a>
+                    <a href="" class="view">View Project &raquo;</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="title">
+                    <h1>Disappr</h1>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
+                    </p>            
+                </div>
+                <div class="links">
+                    <a href=""class="source">Source Code</a>
+                    <a href="" class="view">View Project &raquo;</a>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="card">
-        <div class="title">
-            <h1>Disappr</h1>
-        </div>
-        <div class="text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officia debitis impedit sit nulla. Placeat quia, soluta aspernatur nihil velit reprehenderit laborum cupiditate, accusantium error expedita optio aut repudiandae necessitatibus?
-            </p>            
-        </div>
-        <div class="links">
-            <a href="">Source Code</a>
-            <a href="">View Project</a>
-        </div>
-    </div>
+    <?php  require 'html/footer.php' ?>
 </div>
-<br><br><br><br>
-
-<?php  require 'html/footer.php' ?>
